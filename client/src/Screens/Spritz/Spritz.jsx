c2e828d41b74c9d812de2516d7ca6b83c15a508b
Michael Grieshaber <MichaelJGrieshaber@gmail.com>
2022-02-16 10:38:43 -0600

    added some notes for client meeting

--- client/src/Screens/Spritz/Spritz.jsx
+++ client/src/Screens/Spritz/Spritz.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Spritz() {
-  return <div>Spritz coming soon...</div>;
+  return <div>
+    <div>Spritz coming soon...</div>
+    {/* <div>How to make a Spritz</div> */}
+    {/* <div>Map of spritz ranked, maybe yelp related</div> */}
+    {/* <div>level of bubbly, Aperol, garnish, proseco</div> */}
+  </div>
 }
--- client/src/Screens/Stealz/Stealz.jsx
+++ client/src/Screens/Stealz/Stealz.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Stealz() {
-  return <div>Stealz coming soon...</div>;
+  return <div>
+    <div>Stealz coming soon...</div>
+    {/* <div>links to good stealz</div> */}
+    {/* <div>promo codes to places of you or friends</div> */}
+  </div>
 }
